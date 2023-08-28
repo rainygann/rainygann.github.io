d26ab5bd9ef2f0ee8012317031842217bd7415c5
rainygan <125356911+rainygann@users.noreply.github.com>
2023-08-29 01:25:06 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ let canGenerate = true
 
 async function generateImage(prompt, negativePrompt) {
     if (!canGenerate) return
+    if (document.querySelector('a')) document.querySelector('a').remove()
     canGenerate = false
     document.querySelector('.img-generated img').style= 'display: none'
     notifier.textContent = 'Generating, Please wait...'
-    const response = await fetch('https://api-inference.huggingface.co/models/Ojimi/anime-kawai-diffusion',
+    const response = await fetch('https://api-inference.huggingface.co/models/dreamlike-art/dreamlike-anime-1.0',
         {
             method: "POST",
             headers: {
@@ -26,6 +27,25 @@ async function generateImage(prompt, negativePrompt) {
         document.querySelector('.img-generated img').src = imgUrl
         document.querySelector('.img-generated img').style= 'display: block'
         notifier.textContent = ''
+
+        const downloadImage = document.querySelector(".img-generated img");
+        const downloadButton = document.querySelector(".download-button");
+        const downloadLink = document.createElement("a");
+
+        downloadLink.setAttribute("id", "downloadLink");
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+
+        downloadButton.addEventListener("click", () => {
+            const imageURL = downloadImage.src;
+            const fileName = "ai_generated.jpg";
+
+            downloadLink.href = imageURL;
+            downloadLink.download = fileName;
+
+            downloadLink.click();
+        });
+
     } else if (!response.ok) {
         canGenerate = true
         notifier.textContent = 'Image Failed to Generate.'
